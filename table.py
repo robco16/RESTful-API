f99f4aa18a1c1ceb7525e58542e9bcd3c7028d46
Robert Conti <Robert Conti>
2017-02-19 19:36:56 -0500

    SQLite db

--- table.py
+++ table.py
@@ -4,11 +4,11 @@ connection = sqlite3.connect('data.db')
 
 cursor = connection.cursor()
 
-# MUST BE INTEGER
-# This is the only place where int vs INTEGER matters—in auto-incrementing columns
+# Creates the table that will store the users' id, name, password.
 table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(table)
 
+# Creates the table that will store the name of the tasks and their respective descriptions.
 table = "CREATE TABLE IF NOT EXISTS tasks (name text PRIMARY KEY, description text)" #might need to change that real to text maybe.
 cursor.execute(table)
 
